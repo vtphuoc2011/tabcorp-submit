9b90157f85961b64dee024f1b40439b9b1c1bbfb
Phuoc Van Tran <vtphuoc2011@gmail.com>
2019-03-23 12:05:32 +0700

    Update build server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/tabcorp-demo'));
+app.use(express.static('./dist/tabcorp-submit'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname,'/dist/tabcorp-demo/index.html'));
+res.sendFile(path.join(__dirname,'/dist/tabcorp-submit/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
